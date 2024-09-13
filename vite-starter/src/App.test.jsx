3c685fac0463e65e4d555f7b9d4e1710305bdc6e
Thomas Amlee <thomasamlee@gmail.com>
2024-09-13 13:33:22 -0500

    add unit test

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,43 +1,60 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { kebabCaseToTitleCase } from "./helpers";
 
-test("button click flow", () => {
-  // render app
-  render(<App />);
+describe("flows", () => {
+  test("button click flow", () => {
+    // render app
+    render(<App />);
 
-  // find button
-  const buttonElement = screen.getByRole("button", { name: /blue/i });
+    // find button
+    const buttonElement = screen.getByRole("button", { name: /blue/i });
 
-  // check initial color
-  expect(buttonElement).toHaveClass("red");
+    // check initial color
+    expect(buttonElement).toHaveClass("red");
 
-  // click button
-  fireEvent.click(buttonElement);
+    // click button
+    fireEvent.click(buttonElement);
 
-  // check text
-  expect(buttonElement).toHaveTextContent(/red/i);
+    // check text
+    expect(buttonElement).toHaveTextContent(/red/i);
 
-  // check color
-  expect(buttonElement).toHaveClass("blue");
+    // check color
+    expect(buttonElement).toHaveClass("blue");
+  });
+
+  test("checkbox flow", () => {
+    // render app
+    render(<App />);
+
+    // find button
+    const buttonElement = screen.getByRole("button", { name: /blue/i });
+    // find checkbox
+    const checkboxElement = screen.getByRole("checkbox");
+
+    // check intial states
+    expect(buttonElement).toBeEnabled();
+    expect(checkboxElement).not.toBeChecked();
+
+    // click button
+    fireEvent.click(checkboxElement);
+
+    // check intial states
+    expect(buttonElement).toBeDisabled();
+    expect(checkboxElement).toBeChecked();
+  });
 });
 
-test("checkbox flow", () => {
-  // render app
-  render(<App />);
+describe("kebabCaseToTitleCase", () => {
+  test("works for no hypens", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
 
-  // find button
-  const buttonElement = screen.getByRole("button", { name: /blue/i });
-  // find checkbox
-  const checkboxElement = screen.getByRole("checkbox");
+  test("works for one hyphen", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
 
-  // check intial states
-  expect(buttonElement).toBeEnabled();
-  expect(checkboxElement).not.toBeChecked();
-
-  // click button
-  fireEvent.click(checkboxElement);
-
-  // check intial states
-  expect(buttonElement).toBeDisabled();
-  expect(checkboxElement).toBeChecked();
+  test("works for multiple hyphens", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
