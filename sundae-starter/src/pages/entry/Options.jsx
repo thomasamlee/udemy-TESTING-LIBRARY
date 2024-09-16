5a9f7b249887459e21f8bec5940ca2d6bc9c9386
Thomas Amlee <thomasamlee@gmail.com>
2024-09-16 09:45:35 -0500

    add context to tests

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -3,26 +3,31 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { formatCurrency } from "../../utilities";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
-/**
- * @param {Object} props
- * @param {'toppings' | 'scoops'} props.optionType
- */
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
-  // TODO: replace `null` with ToppingOption when available
+  if (error) {
+    // @ts-ignore
+    return <AlertBanner />;
+  }
+
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
@@ -32,5 +37,14 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
